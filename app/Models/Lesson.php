cb5db2f56f779f2e9a22d33f297980c248cce8f6
Greglib23 <cristianisaias@outlook.com.ar>
2024-03-22 03:03:07 +0000

    Setup of the models

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
 }
