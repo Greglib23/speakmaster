47abe68646cc80f26187a79a614e11ca9d445b60
Greglib23 <cristianisaias@outlook.com.ar>
2024-03-31 08:07:51 +0100

    Full crud for categories, with controller, the pages and components

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -3,15 +3,5 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 </script>
 
 <template>
-    <button v-if="$page.props.user.permissions.includes('create roles')">Create Roles</button>
-    <button v-is="$page - props.user.permissions.includes('create lessons')">Create new Lessons</button>
+    <h1>Hello World!</h1>
 </template>
-
-<style>
-button {
-    background: green;
-    color: white;
-    padding: 20px;
-    margin: 5px;
-}
-</style>
